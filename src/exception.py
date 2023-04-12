933580a5ca0d0ea6de074731e1341b098e9d95f6
DPrakashMewari <prakash.mewari@yahoo.com>
2023-04-12 09:21:41 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging 
+from src.logger import logging
 
 def error_message_detail(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
@@ -15,9 +15,3 @@ class MyException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__ ==  "__main__":
-    try : 
-        a = 1/0 
-    except Exception as e:
-        logging.info("Error Happend")
-        raise MyException(e,sys) 
--- src/logger.py
+++ src/logger.py
@@ -12,5 +12,3 @@ logging.basicConfig(filename=LOG_FILE_PATH,
         format='%(asctime)s- %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO)
 
-if __name__ == "__main__":
-        logging.info("Logging is just started")
